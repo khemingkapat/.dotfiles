322284ced80450fdc85bc7af4c7e0da7286f48a1
KhemIngkapat <60930364+KhemIngkapat@users.noreply.github.com>
2023-01-28 21:01:13 +0700

    remove unused plugin(s)

--- nvim/.config/nvim/lua/packer-plugins.lua
+++ nvim/.config/nvim/lua/packer-plugins.lua
@@ -33,8 +33,9 @@ return require('packer').startup(function(use)
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
   use "hrsh7th/cmp-buffer" -- buffer completions
-  use "hrsh7th/cmp-path" -- path completions
-  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "hrsh7th/cmp-nvim-lsp"
+  -- use "hrsh7th/cmp-path" -- path completions
+  -- use "hrsh7th/cmp-cmdline" -- cmdline completions
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
 
   -- snippets
