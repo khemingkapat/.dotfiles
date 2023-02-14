469f2199d3b15e4b8db946abcb4caa745385a4fc
KhemIngkapat <60930364+KhemIngkapat@users.noreply.github.com>
2023-02-14 20:26:35 +0700

    adding formatting plugin

--- nvim/.config/nvim/lua/packer-plugins.lua
+++ nvim/.config/nvim/lua/packer-plugins.lua
@@ -26,6 +26,7 @@ return require('packer').startup(function(use)
     use 'williamboman/mason.nvim'
     use 'williamboman/mason-lspconfig.nvim'
     use 'neovim/nvim-lspconfig'
+    use 'jose-elias-alvarez/null-ls.nvim' -- formatter
 
     -- terminal
     use "akinsho/toggleterm.nvim"
