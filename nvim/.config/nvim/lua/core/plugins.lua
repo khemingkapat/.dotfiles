0e003e33f2cccc0f9b2588cbe9a3922798e93ae3
KhemIngkapat <60930364+KhemIngkapat@users.noreply.github.com>
2023-03-17 18:26:55 +0700

    adding tmux navigator for tmux

--- nvim/.config/nvim/lua/core/plugins.lua
+++ nvim/.config/nvim/lua/core/plugins.lua
@@ -50,6 +50,8 @@ local plugins = {
   -- snippets
   "L3MON4D3/LuaSnip", --snippet engine
   "rafamadriz/friendly-snippets", -- a bunch of snippets to use
+
+  "christoomey/vim-tmux-navigator"
 }
 
 local opts = {}
