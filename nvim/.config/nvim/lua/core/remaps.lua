cb671bbe8403c624c5189de7d32b1bd02d38cd42
KhemIngkapat <60930364+KhemIngkapat@users.noreply.github.com>
2023-04-06 20:47:51 +0700

    remap wincmd

--- nvim/.config/nvim/lua/core/remaps.lua
+++ nvim/.config/nvim/lua/core/remaps.lua
@@ -1,6 +1,6 @@
 local opts = {noremap=true}
 vim.keymap.set('t','<Esc>','<C-\\><C-n>',opts)
-vim.keymap.set('n','<leader>h',':wincmd h<CR>',opts)
-vim.keymap.set('n','<leader>j',':wincmd j<CR>',opts)
-vim.keymap.set('n','<leader>k',':wincmd k<CR>',opts)
-vim.keymap.set('n','<leader>l',':wincmd l<CR>',opts)
+vim.keymap.set('n','<C-h>',':wincmd h<CR>',opts)
+vim.keymap.set('n','<C-j>',':wincmd j<CR>',opts)
+vim.keymap.set('n','<C-k>',':wincmd k<CR>',opts)
+vim.keymap.set('n','<C-l>',':wincmd l<CR>',opts)
