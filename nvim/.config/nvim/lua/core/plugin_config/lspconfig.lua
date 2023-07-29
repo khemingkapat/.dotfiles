0bf4ea71c2cee15aaceee8e4c90677cef3e0240a
KhemIngkapat <60930364+KhemIngkapat@users.noreply.github.com>
2023-07-29 19:16:42 +0700

    add filetypes to pyright

--- nvim/.config/nvim/lua/core/plugin_config/lspconfig.lua
+++ nvim/.config/nvim/lua/core/plugin_config/lspconfig.lua
@@ -19,7 +19,9 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 require("lspconfig").pyright.setup({
     on_attach = on_attach,
-    capabilities = capabilities
+    capabilities = capabilities,
+    filetypes = {'python'},
+
 
 
 })
