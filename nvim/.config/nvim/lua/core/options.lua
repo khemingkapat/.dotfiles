d2b80b5c54a9fe8ec9c89c845b320ce7e4637dc7
KhemIngkapat <60930364+KhemIngkapat@users.noreply.github.com>
2024-04-24 15:34:09 +0700

    config yanking
    remove auto yank to clipboard and set copy to clipboard

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -26,14 +26,11 @@ o.undofile=true
 
 o.clipboard = "unnamedplus"
 
-if vim.fn.has('wsl') == 1 then
-
-vim.api.nvim_create_autocmd('TextYankPost', {
-
-	group = vim.api.nvim_create_augroup('Yank', { clear = true }),
-
-	callback = function()
-	    vim.fn.system('clip.exe', vim.fn.getreg('"'))
-	end,
-	})
-end
+-- if vim.fn.has('wsl') == 1 then
+--     vim.api.nvim_create_autocmd('TextYankPost', {
+-- 	group = vim.api.nvim_create_augroup('Yank', { clear = true }),
+-- 	callback = function()
+-- 	    vim.fn.system('clip.exe', vim.fn.getreg('"'))
+-- 	end,
+-- 	})
+-- end
