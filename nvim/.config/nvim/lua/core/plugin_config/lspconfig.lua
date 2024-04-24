a1ee34beba780779b6c226414d112ce0196839f3
KhemIngkapat <60930364+KhemIngkapat@users.noreply.github.com>
2024-04-24 19:31:32 +0700

    installing more lsp

--- nvim/.config/nvim/lua/core/plugin_config/lspconfig.lua
+++ nvim/.config/nvim/lua/core/plugin_config/lspconfig.lua
@@ -1,35 +1,36 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-    ensure_installed = {"pyright",
-                        "rust_analyzer",
-                        "clangd",}
+    ensure_installed = { "pyright",
+        "rust_analyzer",
+        "clangd",
+        "lua_ls" }
 })
 
-local on_attach = function(client,bufnr)
+local on_attach = function(client, bufnr)
     local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
 
     -- Mappings
-    local opts = { noremap=true, silent=true }
+    local opts = { noremap = true, silent = true }
 
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
     buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
     buf_set_keymap('n', 'gr', '<cmd>Telescope lsp_references<CR>', opts)
     buf_set_keymap('n', '<space>em', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-    end
+end
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 require("lspconfig").pyright.setup({
     on_attach = on_attach,
     capabilities = capabilities,
-    filetypes = {'python'},
+    filetypes = { 'python' },
 })
 
 require("lspconfig").rust_analyzer.setup({
     on_attach = on_attach,
     capabilities = capabilities,
-    filetypes = {"rust"},
+    filetypes = { "rust" },
     root_dir = require("lspconfig").util.root_pattern("Cargo.toml"),
     settings = {
         ['rust-analyzer'] = {
@@ -44,23 +45,29 @@ require("lspconfig").rust_analyzer.setup({
 require("lspconfig").clangd.setup({
     on_attach = on_attach,
     capabilities = capabilities,
-    filetypes = {'c','cpp'},
+    filetypes = { 'c', 'cpp' },
 })
 
 require("lspconfig").svelte.setup({
     on_attach = on_attach,
     capabilities = capabilities,
-    filetypes = {'svelte'},
+    filetypes = { 'svelte' },
 })
 
 require("lspconfig").cssls.setup({
     on_attach = on_attach,
     capabilities = capabilities,
-    filetypes = {'css'},
+    filetypes = { 'css' },
 })
 
 require("lspconfig").html.setup({
     on_attach = on_attach,
     capabilities = capabilities,
-    filetypes = {'html'},
+    filetypes = { 'html' },
+})
+
+require("lspconfig").lua_ls.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { "lua" }
 })
