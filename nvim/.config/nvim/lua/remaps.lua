b5f5d008c1466edb7cae5e0cc47874695621f0a6
KhemIngkapat <60930364+KhemIngkapat@users.noreply.github.com>
2024-05-10 21:17:09 +0700

    config toggle line wrap

--- nvim/.config/nvim/lua/remaps.lua
+++ nvim/.config/nvim/lua/remaps.lua
@@ -1,3 +1,13 @@
-local opts = {noremap=true}
+locallopts = {noremap=true}
 vim.keymap.set('t','<Esc>','<C-\\><C-n>',opts)
 vim.keymap.set('l','yy','+y',opts)
+
+function _toggle_wrap()
+    if vim.api.nvim_get_option_value("wrap",{}) then
+        vim.cmd("set nowrap")
+    else
+        vim.cmd("set wrap")
+    end
+end
+
+vim.keymap.set('n','<leader>lr',"<cmd>lua _toggle_wrap()<CR>",opts)
