6f558d2f522e6dd46341997ba4ed0acfd7b091fc
KhemIngkapat <60930364+KhemIngkapat@users.noreply.github.com>
2024-08-25 11:14:00 +0700

    oil config

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -5,7 +5,22 @@ return {
     },
     config = function()
         local oil = require("oil")
-        oil.setup()
+        oil.setup({
+            default_file_explorer = true,
+            delete_to_trash = true,
+            skip_confirm_for_simple_edits=true,
+
+            view_options = {
+                show_hidden=true,
+                natural_order=true,
+                is_always_hidden = function (name,_)
+                    return name == '..' or name == '.git'
+                end,
+            },
+            win_options = {
+                wrap=true,
+            }
+        })
         vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
     end,
 }
